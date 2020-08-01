b2694e357dbda0ec8cf604b70820b001b531f29a
TheOnAndOnlyZenomat <eichelstein@online.de>
2020-08-01 12:04:57 +0200

    changed colorscheme

--- config.h
+++ config.h
@@ -90,37 +90,37 @@ char *termname = "st-256color";
 unsigned int tabspaces = 8;
 
 /* bg opacity */
-unsigned int alpha = 0xbb;
+unsigned int alpha = /*0xbb*/0xff;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
 	/* 8 normal colors */
-	[0] = "#282828", /* hard contrast: #1d2021 / soft contrast: #32302f */
-	[1] = "#cc241d", /* red     */
-	[2] = "#98971a", /* green   */
- 	[3] = "#d79921", /* yellow  */
-  	[4] = "#458588", /* blue    */
-  	[5] = "#b16286", /* magenta */
-  	[6] = "#689d6a", /* cyan    */
-  	[7] = "#a89984", /* white   */
+	[0] = "#1c1e26", /* hard contrast: #1d2021 / soft contrast: #32302f */
+	[1] = "#e93c58", /* red     */
+	[2] = "#efaf8e", /* green   */
+ 	[3] = "#efb993", /* yellow  */
+  	[4] = "#df5273", /* blue    */
+  	[5] = "#b072d1", /* magenta */
+  	[6] = "#24a8b4", /* cyan    */
+  	[7] = "#cbced0", /* white   */
 
   	/* 8 bright colors */
-  	[8]  = "#928374", /* black   */
-  	[9]  = "#fb4934", /* red     */
-  	[10] = "#b8bb26", /* green   */
-  	[11] = "#fabd2f", /* yellow  */
-  	[12] = "#83a598", /* blue    */
-  	[13] = "#d3869b", /* magenta */
-  	[14] = "#8ec07c", /* cyan    */
-  	[15] = "#ebdbb2", /* white   */
+  	[8]  = "#6f6f70", /* black   */
+  	[9]  = "#e93c58", /* red     */
+  	[10] = "#efaf8e", /* green   */
+  	[11] = "#efb993", /* yellow  */
+  	[12] = "#df5273", /* blue    */
+  	[13] = "#b072d1", /* magenta */
+  	[14] = "#24a8b4", /* cyan    */
+  	[15] = "#e3e6ee", /* white   */
 
 	[255] = 0,
 
 	/* more colors can be added after 255 to use with DefaultXX */
-	"#add8e6", /* 256 -> cursor */
+	"#cbced0", /* 256 -> cursor */
 	"#555555", /* 257 -> rev cursor*/
-	"#1d2020", /* 258 -> bg */
-	"#ebdbb2", /* 259 -> fg */
+	"#1c1e26", /* 258 -> bg */
+	"#cbced0", /* 259 -> fg */
 };
 
 
